f2ee474d7fe4caddaafde1ff67cc471fc94f0442
Haar.V <haar.vlad@gmx.de>
2025-09-22 12:10:45 +0200

    Logik eingeführt

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+    const [key, setKey] = useState("");
+
+    function check() {
+        alert("Das Password ist: " + key);
+    }
+
+    return (
+        <div className="App">
+            <h1>Check license key</h1>
+            <div className="row-one">
+
+                <div className="row-two">
+                    <label htmlFor="key">Please enter your key: </label>
+                    <br/>
+                    <input
+                        type="text"
+                        id="key"
+                        value={key}
+                        onChange={(e) => setKey(e.target.value)}
+                    />
+                </div>
+
+                <div className="row-three">
+                    <button
+                        className="button-check"
+                        id="button-check"
+                        onClick={check}>
+                        Check
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
